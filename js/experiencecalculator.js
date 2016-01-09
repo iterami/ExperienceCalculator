3b923df35b359e656347b06b0b114e8417746b96
Jan Horava <czechinthemail@gmail.com>
2016-01-09 11:55:48 -0800

    simplified calculate()

--- js/experiencecalculator.js
+++ js/experiencecalculator.js
@@ -4,20 +4,21 @@ function calculate(){
     // Fetch XP value without , or ..
     var xp = parseInt(document.getElementById('xp').value.replace(/(\,|\.)/g, ''));
 
-    // Diablo 2 and Diablo 2: Lord of Destruction
-    document.getElementById('d2lod-99').innerHTML = (xp / 38377390.17).toFixed(7); // Level 99
-
-    // Runescape
-    document.getElementById('rs-99').innerHTML = (xp / 130344.31).toFixed(7); // Level 99
-    document.getElementById('rs-120').innerHTML = (xp / 1042731.67).toFixed(7); // Level 120
-    document.getElementById('rs-max').innerHTML = (xp / 2000000).toFixed(7); // 200 million XP
-    document.getElementById('rs-f2p').innerHTML = (xp / 32000038.8).toFixed(7); // Max F2P XP
-    document.getElementById('rs-p2p').innerHTML = (xp / 52000000).toFixed(7); // Max Member XP
-
-    // Fetch and calculate custom XP goal without decimal mark or thousands separator.
-    document.getElementById('custom-span').innerHTML = (xp /
-      parseInt(document.getElementById('custom').value.replace(/(\,|\.)/g, '')) * 100
-    ).toFixed(7);
+    var ids = {
+      // Diablo 2 and Diablo 2: Lord of Destruction
+      'd2lod-99': 38377390.17,
+      // Runescape
+      'rs-99': 130344.31,
+      'rs-120': 1042731.67,
+      'rs-f2p': 32000038.8,
+      'rs-max': 2000000,
+      'rs-p2p': 52000000,
+      // Custom
+      'custom': parseInt(document.getElementById('custom-xp').value.replace(/(\,|\.)/g, '')) / 100,
+    };
+    for(var id in ids){
+        document.getElementById(id).innerHTML = (xp / ids[id]).toFixed(7);
+    };
 }
 
 window.onkeydown = function(e){
