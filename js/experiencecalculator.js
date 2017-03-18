2985bd2d5ea0d883c595381b21983d2e3988241f
honzi <czechinthemail@gmail.com>
2017-03-18 01:34:30 -0700

    improved formatting and localStorage usage

--- js/experiencecalculator.js
+++ js/experiencecalculator.js
@@ -4,34 +4,36 @@ function calculate(){
     storage_save();
 
     // Fetch XP value without , or ..
-    var xp = parseInt(
-      document.getElementById('xp').value.replace(/(\,|\.)/g, ''),
-      10
-    );
+    var xp = sanitize_integer('xp');
 
     var ids = {
-      // Diablo 2 and Diablo 2: Lord of Destruction
+      // Other
+      'custom': sanitize_integer('custom-xp') / 100,
+      // Diablo 2
       'd2lod-99': 35204852.54,
-      // Runescape
+      // RuneScape
       'rs-99': 130344.31,
       'rs-120': 1042731.67,
       'rs-f2p': 32000038.8,
       'rs-max': 2000000,
       'rs-p2p': 52000000,
-      // Custom
-      'custom': parseInt(
-        document.getElementById('custom-xp').value.replace(/(\,|\.)/g, ''),
-        10
-      ) / 100,
     };
     for(var id in ids){
         document.getElementById(id).innerHTML = (xp / ids[id]).toFixed(7);
     };
 }
 
+function sanitize_integer(id){
+    return parseInt(
+      document.getElementById(id).value.replace(/(\,|\.)/g, ''),
+      10
+    );
+}
+
 window.onload = function(e){
     storage_init({
       'data': {
+        'custom-xp': '0',
         'xp': '0',
       },
       'prefix': 'ExperienceCalculator.htm-',
