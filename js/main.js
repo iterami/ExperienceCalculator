b40176b890328693d1b013c634c3ee9485e265d5
honzi <czechinthemail@gmail.com>
2017-10-24 22:09:43 -0700

    moved event binds to events arg

--- js/main.js
+++ js/main.js
@@ -2,15 +2,20 @@
 
 function repo_init(){
     core_repo_init({
+      'events': {
+        'custom-xp': {
+          'oninput': calculate,
+        },
+        'xp': {
+          'oninput': calculate,
+        },
+      },
       'storage': {
         'custom-xp': '0',
         'xp': '0',
       },
       'title': 'ExperienceCalculator.htm',
     });
-
-    document.getElementById('custom-xp').oninput = calculate;
-    document.getElementById('xp').oninput = calculate;
 
     core_storage_update();
     calculate();
